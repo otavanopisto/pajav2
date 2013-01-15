957a69a67e83c5ac28c7904253b3c171ab9617b3
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-01-15 09:42:31 +0200

    ** The mighty removal on those pesky .tabs and .submitted elements from page nodes ** Added marvelous graphics for viewers pleasure

---
+++ sites/default/themes/burarum/templates/page.tpl.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * @file
+ * Alpha's theme implementation to display a single Drupal page.
+ */
+?>
+<div<?php print $attributes; ?>>
+  <div id="page-container">
+    <div class="header-black-spacer"></div>
+  <?php if (isset($page['header'])) : ?>
+    <?php print render($page['header']); ?>
+  <?php endif; ?>
+  
+  <?php if (isset($page['content'])) : ?>
+    <?php print render($page['content']); ?>
+  <?php endif; ?>  
+  
+  <?php if (isset($page['footer'])) : ?>
+    <?php print render($page['footer']); ?>
+  <?php endif; ?>
+  </div>
+</div>
