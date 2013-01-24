a61102cb8ef5c3aded1ccecde472ee0827e77268
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-01-24 08:51:03 +0200

    ** Updated content types feature ** Added some nifty styles too :-)

--- sites/default/themes/burarum/js/script.js
+++ sites/default/themes/burarum/js/script.js
@@ -21,14 +21,43 @@
   Drupal.behaviors.ShowFullDescription = {
     attach: function(context, settings) {
       
+      var descControlWrapper = $('.desc-control-wrapper');
+      var showMoreLink = $('.show-more-link');
+      var showLessLink = $('.show-less-link');
       var descElementWrapper = $('.pane-node-body');
       var descELementHeight = $('.field-name-body').outerHeight();
       
-      descElementWrapper.click(function() {
+      if (descELementHeight >= '400') {
+	    showMoreLink.click(function() {
+	    	descElementWrapper.animate({
+	    	  height: descELementHeight + 'px'
+	    	  }, 500, 'linear', function() {
+	            showMoreLink.addClass('hiddenControl');
+	            showLessLink.removeClass('hiddenControl');
+	        });
+	      });
+      } 
+      else {
+    	descControlWrapper.css({
+    	  display:'none'
+        });
+      }
+    }
+  };
+  
+  Drupal.behaviors.ShowLessDescription = {
+    attach: function(context, settings) {
+      
+      var showMoreLink = $('.show-more-link');
+      var showLessLink = $('.show-less-link');
+      var descElementWrapper = $('.pane-node-body');
+      
+      showLessLink.click(function() {
     	descElementWrapper.animate({
-    	  height: descELementHeight + 15 + 'px'
-    	  }, 500, 'linear',  function() {
-
+    	  height: '400px'
+    	  }, 500, 'linear', function() {
+            showMoreLink.removeClass('hiddenControl');
+            showLessLink.addClass('hiddenControl');
         });
       });
       
