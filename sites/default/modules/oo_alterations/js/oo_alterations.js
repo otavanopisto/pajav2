862a76d786d471c2d9959c6a3ba8de9da6ba0ea8
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-02-13 13:23:36 +0200

    Subscribe to Og separeted from here.

---
+++ sites/default/modules/oo_alterations/js/oo_alterations.js
@@ -0,0 +1,16 @@
+(function($) {
+  Drupal.behaviors.comment_ajax_login = {
+    attach : function(context, settings) {
+      var commentAjaxLink = $('.comment-ajax-login');
+      var comment_ajax_settings = {};
+      comment_ajax_settings.submit = {target : '.generic-empty-text-wrapper'};
+      comment_ajax_settings.url = commentAjaxLink.attr('href');
+      comment_ajax_settings.event = 'click tap';
+      var ajax = new Drupal.ajax('.comment-ajax-login', commentAjaxLink, comment_ajax_settings);
+      // Make a copy of the original function so we can still call
+      // it.
+      ajax.old_success = ajax.success;
+      
+    }
+  };
+})(jQuery);
