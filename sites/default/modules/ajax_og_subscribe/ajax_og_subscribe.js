8fb187f9b425274474913347fb5526fc796e6d28
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-02-14 15:04:37 +0200

    ** Calcel no more ... and other magnificent stuff

--- sites/default/modules/ajax_og_subscribe/ajax_og_subscribe.js
+++ sites/default/modules/ajax_og_subscribe/ajax_og_subscribe.js
@@ -11,16 +11,17 @@
       }
     }
   };
-  
-  Drupal.behaviors.calcelCourseRegisterLink = {
+  // Override default Cancel link behaviour ...
+  Drupal.behaviors.cancelCourseRegisterLink = {
     attach: function(context, settings) {
     	if ($('#join-course-form-block').length > 0) {
     		$('#join-course-form-block a#edit-cancel').click(function(event) {
-    			$('#join-course-form-block').remove();
+    			$('#join-course-form-block').fadeOut(300, function () {
+    				$(this).remove();
+    			});
     			event.preventDefault();
 			});
     	}
     } 
   };
-  
 })(jQuery);
