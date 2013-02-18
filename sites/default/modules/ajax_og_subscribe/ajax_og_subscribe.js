034cc19c51e0be014cc0fc57199ef54afea4015b
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-02-18 15:10:40 +0200

    **

--- sites/default/modules/ajax_og_subscribe/ajax_og_subscribe.js
+++ sites/default/modules/ajax_og_subscribe/ajax_og_subscribe.js
@@ -3,30 +3,30 @@
     attach : function(context, settings) {
       if ($('#join-course-bt').length > 0) {
         $('#join-course-bt').click(function(event){
-          if ($('#join-course-form-block').length > 0) {
-            $('#join-course-form-block').empty();
+          if ($('#join-course-form-block')) {
             $('#join-course-form-block').remove();
-            event.preventDefault();
-            event.stopPropagation();
+            
           }
+          event.preventDefault();
+          event.stopPropagation();
         });
       }
     }
   };
 
   Drupal.behaviors.handlePopupClose = {  
-      attach : function(context, settings) {
-        if ($('#join-course-form-block').length > 0) {
-          $('#join-course-form-block').click(function(event){
-             event.stopPropagation();
-          });
-          $('.html').click(function(event){
-            $('#join-course-form-block').empty();
-            $('#join-course-form-block').remove();
-          });
-        }
-      }
-    };
+	  attach : function(context, settings) {
+	    if ($('#join-course-form-block').length > 0) {
+	      $('#join-course-form-block').click(function(event){
+	         event.stopPropagation();
+	      });
+	      $('.html').click(function(event){
+	        $('#join-course-form-block').empty();
+	        $('#join-course-form-block').remove();
+	      });
+	    }
+	  }
+	};
   
   Drupal.behaviors.subscribeAjaxLogin = {
     attach : function(context, settings) {
@@ -40,7 +40,7 @@
       }
     }
   };
-  // Override default Cancel link behaviour ...
+  // Override default Cancel link behavior ...
   Drupal.behaviors.cancelCourseRegisterLink = {
     attach: function(context, settings) {
     	if ($('#join-course-form-block').length > 0) {
