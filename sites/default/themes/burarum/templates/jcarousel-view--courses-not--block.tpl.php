b80729a2f6f584a5f901fe6b952440b1f6505c02
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-04-09 13:30:20 +0300

    Phiiix

--- sites/default/themes/burarum/templates/jcarousel-view--courses-not--block.tpl.php
+++ sites/default/themes/burarum/templates/jcarousel-view--courses-not--block.tpl.php
@@ -7,7 +7,7 @@
 ?>
 <ul class="<?php print $jcarousel_classes; ?>">
   <?php foreach ($rows as $id => $row): ?>
-    <li class="<?php print $row_classes[$id]; ?>"><?php print $row; ?></li>
+    <li class="<?php print $row_classes[$id]; ?>"><div class="jcarousel-item-padding"><?php print $row; ?></div></li>
   <?php endforeach; ?>
   <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-<?php print $id+1; ?> jcarousel-item-<?php print $id+1; ?>-horizontal" style="float: left; list-style: none outside none;" jcarouselindex="<?php print $id+1; ?>">
     <div class="jcarousel-item-padding">  
@@ -22,6 +22,7 @@
       <div class="views-field views-field-field-summary">
         <span class="field-content">Perusta sinua kiinnostavasta aiheesta oma kurssisi yhdessä muiden kiinnostuneiden kanssa. Samasta aiheesta kiinnostuneet ideoivat ja suunnittelevat kurssin yhdessä.</span>
       </div>
+      <div class="views-field views-field-nothing-2"><span class="field-content"></span></div>
     </div>
   </li>
 </ul>
