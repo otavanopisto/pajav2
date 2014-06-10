eaab018d0b6c39f5474640ffceea19bc339d4fde
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2014-06-10 12:10:56 +0300

    Templates for new course view

--- sites/default/themes/burarum/templates/views-view-field--courses-not--nothing-1.tpl.php
+++ sites/default/themes/burarum/templates/views-view-field--courses-not--nothing-1.tpl.php
@@ -22,7 +22,15 @@
  */
 ?>
 <?php
-  $members = filter_xss($row->views_php_12);
+// $attendee_query_result = db_query("SELECT field_hintalaskuri_attendees_max FROM {field_data_field_hintalaskuri} field_data_field_hintalaskuri WHERE entity_id = :gid;", array(':gid' => $gid));
+$query1 = db_query("SELECT COUNT(nid) AS c FROM {webform_submissions} webform_submissions WHERE (webform_submissions.nid = :nid) AND (webform_submissions.is_draft = 0)", array(":nid" => $row->nid));
+$members = 0;
+foreach ($query1 as $qr){
+  $members = $qr->c;
+}
+// $result1 = $query1->execute();
+// $admin_count = $result1->rowCount();
+//    = filter_xss($row->views_php_7);
   $min_members = 0;
   if(isset($row->field_field_hintalaskuri[0]))
     $min_members = filter_xss($row->field_field_hintalaskuri[0]['rendered']['#markup']);
@@ -30,6 +38,6 @@
   if($min_members != 0)
     $attendance = round(min(array(($members/$min_members) * 100, 100)),0);
   $output = '<div class="attendancies-indicator-wrapper"><div style="width:'.$attendance.'%;" class="attendancies-indicator"><div class="attendancies-percent">'.$attendance.'%</div></div></div>';
-  print $output;
+  print $output;  
 ?>
 
